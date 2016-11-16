b9e6c48fa684590bdab341acd4e9c9117056c2cd
siyehua <624104244>
2016-11-16 18:11:44 +0800

    init

---
+++ app/src/main/java/com/siyehua/threedshopping/ItemFragment.java
@@ -0,0 +1,16 @@
+package com.siyehua.threedshopping;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by huangxk on 2016/11/16.
+ */
+public class ItemFragment extends Fragment {
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+//        Bitmap bitmap =
+    }
+}
